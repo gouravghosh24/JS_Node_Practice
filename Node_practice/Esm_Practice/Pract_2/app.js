081bf2d91ebcfca6876be987ea43f7a82921d3f8
Gourav Ghosh <84674152+gouravghosh24@users.noreply.github.com>
2021-12-05 13:11:15 +0530

    Practice Two

---
+++ Node_practice/Esm_Practice/Pract_2/app.js
@@ -0,0 +1,6 @@
+import datas from './main.js'
+import {Para} from './main.js'
+import {Data as empdata} from './main.js'
+console.log(datas)
+console.log(Para.about)
+console.log(empdata)
---
+++ Node_practice/Esm_Practice/Pract_2/main.js
@@ -0,0 +1,24 @@
+export default function Node_export_import_Pract(){
+    console.log("I am a function")
+}
+export const Para ={
+    about:`This is basically a code where I am
+    trying to figure out in how many ways we can
+    try to export and import modules in ES Modules ES6`
+}
+export const Data={
+    emp:{
+        name:"Javed Bashir",
+        age:30,
+        height: "167cm"
+
+    },
+    dept:{
+        block: "B"
+    },
+    salary:{
+        minim:30000,
+        maxim:150000
+    }
+
+}
